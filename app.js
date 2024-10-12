9a1522880858d5e47dfa75589a57d07e3707ad0a
apoudatta <apoudatto6@gmail.com>
2024-10-12 21:05:23 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log('hey node!');
+
+// setInterval
+// var time = 0;
+// var timer = setInterval(() => {
+//     time += 2;
+//     console.log(time + ' second have passed');
+//     if (time > 5) {
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+console.log(__dirname);
+console.log(__filename);
